f3b5ded5035bba152a80ab29cd836d5221fb0d21
Jordan Knight <jorkni@microsoft.com>
2017-02-10 00:30:06 +1100

    Work in progress. Most stuff dumped in, not working

--- PrettyJsonApi/Startup.cs
+++ PrettyJsonApi/Startup.cs
@@ -2,23 +2,40 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace PrettyJsonApi
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+           
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<SigningSettings>(Configuration.GetSection("SigningSettings"));
             services.AddRouting();
         }
 
@@ -33,14 +50,49 @@ namespace PrettyJsonApi
             }
 
             app.UseStaticFiles();
+            var secretKey = "sdfsdfsd";
+            var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                // The signing key must match!
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = key,
+
+                // Validate the JWT Issuer (iss) claim
+                ValidateIssuer = true,
+                ValidIssuer = "ExampleIssuer",
+
+                // Validate the JWT Audience (aud) claim
+                ValidateAudience = true,
+                ValidAudience = "ExampleAudience",
+
+                // Validate the token expiry
+                ValidateLifetime = true,
+
+                // If you want to allow a certain amount of clock drift, set that here:
+                ClockSkew = TimeSpan.Zero
+            };
+
+            app.UseJwtBearerAuthentication(new JwtBearerOptions
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                TokenValidationParameters = tokenValidationParameters
+            });
+
+            //var sKey = new RsaSecurityKey();
+            //var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
 
             app.UseRouter(router =>
             {
-                router.MapPost("pretty", async (request, response, RouteData) =>
+                router.MapPost("pretty/{language}", async (request, response, routeData) =>
                 {
                     using (var streamReader = new StreamReader(request.HttpContext.Request.Body))
                     {
                         var json = await streamReader.ReadToEndAsync();
+
+                        var language = routeData.Values["language"];
 
                         var basePath = $"{request.Scheme}://{request.Host}";
 
@@ -52,10 +104,10 @@ namespace PrettyJsonApi
                                 </head>
                                 <body>	
 	                                <script src=""{0}/prism.js""></script>
-                                    <pre><code class=""language-csharp"">{1}</code></pre>
+                                    <pre><code class=""language-{2}"">{1}</code></pre>
                                 </body>
                                 </html>";
-                        var result = string.Format(pattern, basePath, json);
+                        var result = string.Format(pattern, basePath, json, language);
 
                         await response.WriteAsync(result);
                     }
